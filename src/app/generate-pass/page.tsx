c62450706ffb269133cf9506de408d692d0ff30e
José Galdámez <josegaldamez1991@gmail.com>
2024-10-08 11:36:38 -0600

    init project

---
+++ src/app/generate-pass/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GeneratePassPage = () => {
+    return (
+        <div>
+            <h1>WhatPass</h1>
+            <p>Manage your passwords</p>
+        </div>
+    )
+}
+
+export default GeneratePassPage;
