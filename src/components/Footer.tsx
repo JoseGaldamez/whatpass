fd2cda6a20434cc0dbd98ea447f4719d85c01623
José Galdámez <josegaldamez1991@gmail.com>
2024-10-08 19:25:31 -0600

    author link open other tab

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
     return (
         <div className='fixed w-full p-5 text-center bottom-0 bg-slate-200 z-10'>
-            <a className='underline text-slate-500' href="https://josegaldamez.dev">josegaldamez.dev</a>
+            <a target='_blank' className='underline text-slate-500' href="https://josegaldamez.dev">josegaldamez.dev</a>
         </div>
     )
 }
